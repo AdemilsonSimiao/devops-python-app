fe826aa42c887993f3fdf64433bb8689f1237560
AdemilsonSimiao <biassistencia@gmail.com>
2025-04-12 21:41:19 -0300

    fix: set host

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
@@ -5,5 +6,6 @@ def hello_world():
     return 'Hello, World! This is a simple Flask application.'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 # This code creates a simple Flask web application that returns "Hello, World! This is a simple Flask application." when accessed at the root URL.
